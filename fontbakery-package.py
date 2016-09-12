a593f919c5065f017f6389515b60a24f1ab552c8
Marc Foley <m.foley.88@gmail.com>
2016-09-12 11:00:34 +0200

    package: moved argparse out of main()

--- fontbakery-package.py
+++ fontbakery-package.py
@@ -14,13 +14,12 @@ import importlib
 fb_check = importlib.import_module("fontbakery-check-ttf")
 
 
-def main():
+def main(path):
+    print(path)
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Package upstream repo")
     parser.add_argument('source', metavar='s', type=str,
                          help='Add source folder')
     args = parser.parse_args()
-    print(args.source)
-
-
-if __name__ == "__main__":
-    main()
+    main(args.source)
